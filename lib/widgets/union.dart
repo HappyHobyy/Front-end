ea417ada4c0cee73336e41b7153274b1b759ace1
alicek0 <alicekangisy@gmail.com>
2024-05-19 15:57:14 +0900

    Feature: Added tags page that can be accesed through Union - Page can be created with TagPage() - Search bar fully functional - Temporary tags bar that displays selected tags - Yet to be connected with hobbyImageMap (show correct image icon per hobby)

--- lib/widgets/union.dart
+++ lib/widgets/union.dart
@@ -2,9 +2,9 @@ import 'dart:ffi';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hobbyhobby/Union/create_union.dart';
+import 'package:hobbyhobby/Union/tag_page.dart';
 import 'package:hobbyhobby/constants.dart';
 import 'package:page_transition/page_transition.dart';
-
 
 class UnionPage extends StatefulWidget {
   const UnionPage({super.key});
@@ -13,24 +13,24 @@ class UnionPage extends StatefulWidget {
   State<UnionPage> createState() => _UnionPageState();
 }
 
-class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMixin{
+class _UnionPageState extends State<UnionPage>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
   }
 
   @override
-  void dispose(){
+  void dispose() {
     _tabController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -45,88 +45,93 @@ class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMix
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: (){
+            onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute<Widget>(builder: (BuildContext context) {
                   return Scaffold(
-                    appBar: AppBar(
-                      title: const Text(
-                        '모임 생성',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
+                      appBar: AppBar(
+                        title: const Text(
+                          '모임 생성',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
-                    ),
-                    body: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                                height: 600,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(8),
-                                  boxShadow: [BoxShadow(
-                                    color: Colors.grey.withOpacity(0.4),
-                                    spreadRadius: 1,
-                                    blurRadius: 3,
-                                    offset: Offset(0, 3), // changes position of shadow
-                                    ),
-                                  ],
-                                ),
-                                child: const Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Row(
-                                    children: [
-                                      const SizedBox(height: 30, width: 10,),
-                                      Text(
-                                        '약관 및 주의사함',
-                                        style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 10,
-                                        ),
+                      body: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  margin:
+                                      const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                                  height: 600,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(8),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.4),
+                                        spreadRadius: 1,
+                                        blurRadius: 3,
+                                        offset: Offset(
+                                            0, 3), // changes position of shadow
                                       ),
                                     ],
-                                  )
-                                ),
-                              ),
-                              const SizedBox(height: 30),
-                              InkWell(
-                                onTap: () async {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => CreateUnion()),
-                                  );
-                                },
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Constants.primaryColor,
-                                    borderRadius: BorderRadius.circular(30),
                                   ),
-                                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                                  child: Center(
-                                    child: Text(
-                                      '동의하기',
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 18.0,
+                                  child: const Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Row(
+                                        children: [
+                                          const SizedBox(
+                                            height: 30,
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            '약관 및 주의사함',
+                                            style: TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 10,
+                                            ),
+                                          ),
+                                        ],
+                                      )),
+                                ),
+                                const SizedBox(height: 30),
+                                InkWell(
+                                  onTap: () async {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => CreateUnion()),
+                                    );
+                                  },
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Constants.primaryColor,
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 10, vertical: 15),
+                                    child: Center(
+                                      child: Text(
+                                        '동의하기',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18.0,
+                                        ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        )
-                    )
-                  );
+                              ],
+                            ),
+                          )));
                 }),
               );
             },
@@ -139,9 +144,9 @@ class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMix
         ],
         bottom: TabBar(
           controller: _tabController,
-          tabs: const <Widget> [
-            Tab(text : '연합 모임'),
-            Tab(text : '단일 모임'),
+          tabs: const <Widget>[
+            Tab(text: '연합 모임'),
+            Tab(text: '단일 모임'),
           ],
         ),
       ),
@@ -156,32 +161,22 @@ class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMix
                 width: 350,
                 alignment: Alignment.topLeft,
                 decoration: BoxDecoration(
-                    color: Colors.white10,
-                    borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white10,
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
                 child: TextButton(
-                  child: Text('+ 태그 추가하기',
-                      style: TextStyle(
-                          fontSize: 11,
-                      ),
+                  child: Text(
+                    '+ 태그 추가하기',
+                    style: TextStyle(
+                      fontSize: 11,
+                    ),
                   ),
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute<Widget>(builder: (BuildContext context) {
-                        return Scaffold(
-                          appBar: AppBar(
-                              title: const Text(
-                                '태그',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                          ),
-                          body: Center(),
-                        );
+                      MaterialPageRoute<Widget>(
+                          builder: (BuildContext context) {
+                        return TagPage();
                       }),
                     );
                   },
@@ -195,21 +190,29 @@ class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMix
                 title: Text('연합 모임 예시_1'),
                 subtitle: Row(
                   children: [
-                    const SizedBox(width: 0,),
+                    const SizedBox(
+                      width: 0,
+                    ),
                     Text('풋살킹'),
-                    const SizedBox(width: 20,),
+                    const SizedBox(
+                      width: 20,
+                    ),
                     Icon(
                       Icons.account_circle_sharp,
                       size: 15,
                     ),
-                    const SizedBox(width: 20,),
+                    const SizedBox(
+                      width: 20,
+                    ),
                     Text(
                       '# 풋살',
                       style: TextStyle(
                         fontSize: 12,
                       ),
                     ),
-                    const SizedBox(width: 5,),
+                    const SizedBox(
+                      width: 5,
+                    ),
                     Text(
                       '# 사진',
                       style: TextStyle(
@@ -222,7 +225,7 @@ class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMix
                   padding: const EdgeInsets.all(0.0),
                   child: Text('09:41'),
                 ),
-                onTap: (){
+                onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute<Widget>(builder: (BuildContext context) {
@@ -242,7 +245,7 @@ class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMix
                 title: Text('Item1-2'),
                 subtitle: Text('Item description'),
                 trailing: Text('date'),
-                onTap: (){
+                onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute<Widget>(builder: (BuildContext context) {
@@ -262,7 +265,7 @@ class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMix
                 title: Text('Item1-3'),
                 subtitle: Text('Item description'),
                 trailing: Text('date'),
-                onTap: (){
+                onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute<Widget>(builder: (BuildContext context) {
@@ -288,7 +291,8 @@ class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMix
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 child: TextButton(
-                  child: Text('+ 태그 추가하기',
+                  child: Text(
+                    '+ 태그 추가하기',
                     style: TextStyle(
                       fontSize: 11,
                     ),
@@ -296,20 +300,9 @@ class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMix
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute<Widget>(builder: (BuildContext context) {
-                        return Scaffold(
-                          appBar: AppBar(
-                            title: const Text(
-                              '태그',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          body: Center(),
-                        );
+                      MaterialPageRoute<Widget>(
+                          builder: (BuildContext context) {
+                        return TagPage();
                       }),
                     );
                   },
@@ -323,7 +316,7 @@ class _UnionPageState extends State<UnionPage> with SingleTickerProviderStateMix
                 title: Text('Item2-1'),
                 subtitle: Text('Item description'),
                 trailing: Text('date'),
-                onTap: (){
+                onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute<Widget>(builder: (BuildContext context) {
