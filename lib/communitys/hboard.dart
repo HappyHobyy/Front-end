7b61776339f08cbf0be1f8531ce6cfd33cbe2984
alicek0 <alicekangisy@gmail.com>
2024-06-03 00:48:23 +0900

    fix: got rid of back button in hboard page

--- lib/communitys/hboard.dart
+++ lib/communitys/hboard.dart
@@ -19,6 +19,7 @@ class _HboardPageState extends State<HboardPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text(
           widget.communityName,
           style: Constants.titleTextStyle,
