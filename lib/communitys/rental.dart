3ec7a1114efd498b5f45b182e715f27391ef1b4b
sssanghn <ryrymel35@gmail.com>
2024-06-04 01:45:59 +0900

    rental Page update

--- lib/communitys/rental.dart
+++ lib/communitys/rental.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hobbyhobby/constants.dart';
+import 'package:intl/intl.dart';
 
 class RentalPage extends StatefulWidget {
   const RentalPage({super.key});
@@ -201,7 +203,7 @@ class Detail1Page extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                height: 400, // Set the desired height
+                height: 290, // Set the desired height
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(12.0), // Rounded corners
@@ -244,7 +246,7 @@ class Detail1Page extends StatelessWidget {
                 ' 상품설명',
                 style: TextStyle(
                   color: Colors.grey,
-                    fontSize: 16,
+                  fontSize: 16,
                 ),
               ),
               const SizedBox(height: 3),
@@ -335,16 +337,24 @@ class Detail1Page extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(bottom: 30.0, left: 25, right: 25, top: 20),
+        padding: const EdgeInsets.only(bottom: 30.0, left: 25, right: 25, top: 16),
         child: ElevatedButton(
           onPressed: () {
-            // Add your rental functionality here
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (BuildContext context) {
+                return RentalBottomSheet(rentalRate: 50000);
+              },
+            );
           },
-          child: Text('대여하기', style: TextStyle(color: Colors.white,
-          fontSize: 20)),
           style: ElevatedButton.styleFrom(
             backgroundColor: Constants.textColor,
           ),
+          child: Padding(
+            padding: EdgeInsets.only(top:10,bottom: 10),
+          child: Text('대여하기', style: TextStyle(color: Colors.white, fontSize: 20)),
+        ),
         ),
       ),
     );
@@ -504,16 +514,283 @@ class Detail2Page extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(bottom: 30.0, left: 25, right: 25, top: 20),
+        padding: const EdgeInsets.only(bottom: 30.0, left: 25, right: 25, top: 16),
         child: ElevatedButton(
           onPressed: () {
-            // Add your rental functionality here
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (BuildContext context) {
+                return RentalBottomSheet(rentalRate: 100000);
+              },
+            );
           },
-          child: Text('대여하기', style: TextStyle(color: Colors.white,
-              fontSize: 20)),
           style: ElevatedButton.styleFrom(
             backgroundColor: Constants.textColor,
           ),
+          child: Padding(
+            padding: EdgeInsets.only(top:10,bottom: 10),
+            child: Text('대여하기', style: TextStyle(color: Colors.white, fontSize: 20)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RentalBottomSheet extends StatefulWidget {
+  final int rentalRate;
+
+  RentalBottomSheet({required this.rentalRate});
+
+  @override
+  _RentalBottomSheetState createState() => _RentalBottomSheetState();
+}
+
+class _RentalBottomSheetState extends State<RentalBottomSheet> {
+  DateTime selectedStartDate = DateTime.now();
+  DateTime selectedEndDate = DateTime.now().add(Duration(days: 1));
+  TimeOfDay selectedTime = TimeOfDay(hour: 12, minute: 0);
+  late int rentalRate;
+
+  @override
+  void initState() {
+    super.initState();
+    rentalRate = widget.rentalRate;
+  }
+
+  void _showCupertinoDatePicker(BuildContext context, bool isStartDate) {
+    final DateTime minimumDate = DateTime(2024, 1, 1);
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 250,
+          child: CupertinoDatePicker(
+            initialDateTime: isStartDate ? selectedStartDate : selectedEndDate,
+            minimumDate: minimumDate,
+            mode: CupertinoDatePickerMode.date,
+            onDateTimeChanged: (DateTime newDateTime) {
+              setState(() {
+                if (isStartDate) {
+                  selectedStartDate = newDateTime.isBefore(minimumDate) ? minimumDate : newDateTime;
+                  selectedEndDate = selectedStartDate.add(Duration(days: 1));
+                } else {
+                  selectedEndDate = newDateTime.isBefore(selectedStartDate) ? selectedStartDate.add(Duration(days: 1)) : newDateTime;
+                }
+              });
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  void _showCupertinoTimePicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 250,
+          child: CupertinoDatePicker(
+            initialDateTime: DateTime(
+              2020,
+              1,
+              1,
+              selectedTime.hour,
+              0,
+            ),
+            mode: CupertinoDatePickerMode.time,
+            onDateTimeChanged: (DateTime newDateTime) {
+              setState(() {
+                selectedTime = TimeOfDay(
+                  hour: newDateTime.hour,
+                  minute: 0,
+                );
+              });
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  String _formatTimeOfDay(TimeOfDay timeOfDay) {
+    final period = timeOfDay.period == DayPeriod.am ? '오전' : '오후';
+    final hours = (timeOfDay.hourOfPeriod == 0 ? 12 : timeOfDay.hourOfPeriod).toString().padLeft(2, '0');
+    return "$period $hours:00";
+  }
+
+  String _formatCurrency(int amount) {
+    final NumberFormat formatter = NumberFormat('#,###');
+    return formatter.format(amount);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime startDateTime = DateTime(
+      selectedStartDate.year,
+      selectedStartDate.month,
+      selectedStartDate.day,
+      selectedTime.hour,
+      0,
+    );
+
+    DateTime endDateTime = DateTime(
+      selectedEndDate.year,
+      selectedEndDate.month,
+      selectedEndDate.day,
+      selectedTime.hour,
+      0,
+    );
+
+    final rentalPeriodInHours = endDateTime.difference(startDateTime).inHours;
+    final rentalCost = ((rentalPeriodInHours / 24) * rentalRate).toInt(); // Example cost calculation based on 24 hours
+    final formattedRentalCost = _formatCurrency(rentalCost);
+
+    return FractionallySizedBox(
+      heightFactor: 0.45,
+      widthFactor: 1,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(36.0),
+                border: Border.all(
+                  color: Colors.grey,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  '대여시간 선택하기',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '대여일',
+                  style: TextStyle(fontSize: 20),
+                ),
+                GestureDetector(
+                  onTap: () => _showCupertinoDatePicker(context, true),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    child: Text(
+                      "${selectedStartDate.toLocal()}".split(' ')[0],
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => _showCupertinoTimePicker(context),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    child: Text(
+                      _formatTimeOfDay(selectedTime),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ),
+            Container(
+              height: 1,
+              width: 360,
+              color: Colors.grey,
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '반납일',
+                  style: TextStyle(fontSize: 20),
+                ),
+                GestureDetector(
+                  onTap: () => _showCupertinoDatePicker(context, false),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    child: Text(
+                      "${selectedEndDate.toLocal()}".split(' ')[0],
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => _showCupertinoTimePicker(context),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    child: Text(
+                      _formatTimeOfDay(selectedTime),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ),
+            Container(
+              height: 1,
+              width: 360,
+              color: Colors.grey,
+            ),
+            const Spacer(),
+            Padding(
+                padding: EdgeInsets.only(left: 20, right:20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '${rentalPeriodInHours}시간',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                ),
+                Text(
+                  '${formattedRentalCost}원',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            ),
+            const SizedBox(height: 35),
+            ElevatedButton(
+              onPressed: () {
+                // Add your rental functionality here
+                Navigator.pop(context); // Close the bottom sheet
+              },
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 130),
+                child: Text(
+                  '대여하기',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Constants.textColor,
+              ),
+            ),
+            const SizedBox(height: 15),
+          ],
         ),
       ),
     );
